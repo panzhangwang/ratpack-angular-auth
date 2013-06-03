f9d9ace820da0743d694514b574d82c5448b883f
Luke Daley <ld@ldaley.com>
2013-06-03 10:40:54 +0100

    Make templates reloadable during development.

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,3 +1,5 @@
+import org.ratpackframework.groovy.bootstrap.RatpackScriptApp
+import org.ratpackframework.groovy.templating.TemplatingModule
 import org.ratpackframework.session.Session
 import org.ratpackframework.session.store.MapSessionsModule
 import org.ratpackframework.session.store.SessionStorage
@@ -8,7 +10,13 @@ import static org.ratpackframework.groovy.RatpackScript.ratpack
 
 ratpack {
     def logger = Logger.getLogger("")
+
     modules {
+        // Make templates reloadable if we are in reload mode, otherwise cache them
+        def isReloadable = System.getProperty(RatpackScriptApp.Property.RELOADABLE)
+        get(TemplatingModule).cacheSize = isReloadable ? 0 : 100
+
+        // Enable non persistent map based session storage
         register(new MapSessionsModule(10, 5))
     }
 
