fcd288fb7092c60440b14a9373dbb008a315d02b
panzhangwang <pz_wang@hotmail.com>
2013-06-03 09:35:38 +0800

    Add simple log, will create LoggingModule later

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -2,10 +2,12 @@ import org.ratpackframework.session.Session
 import org.ratpackframework.session.store.MapSessionsModule
 import org.ratpackframework.session.store.SessionStorage
 
+import java.util.logging.Logger
 import static groovy.json.JsonOutput.toJson
 import static org.ratpackframework.groovy.RatpackScript.ratpack
 
 ratpack {
+    def logger = Logger.getLogger("")
     modules {
         register(new MapSessionsModule(10, 5))
     }
@@ -28,7 +30,8 @@ ratpack {
         prefix("auth") {
             post("login") {
                 def form = request.form
-                if (form.username == "user" && form.password == "pass") {
+                if (form.username == "user" && form.password == "pass") {                    
+    				logger.info 'User Logged in successfully.'
                     get(SessionStorage).auth = true
                     response.send "application/json", toJson("")
                 } else {
