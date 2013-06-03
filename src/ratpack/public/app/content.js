65663f49a5403455c13a0c4488186b16075975a5
panzhangwang <pz_wang@hotmail.com>
2013-06-03 13:13:44 +0800

    Display error when post failed.

--- src/ratpack/public/app/content.js
+++ src/ratpack/public/app/content.js
@@ -14,7 +14,10 @@ angular.module('angular-auth-demo').controller({
       $http.post('apps/protect', $scope.restrictedData).success(function(response) {
         // this piece of code will not be executed until user is authenticated
         $scope.restrictedContent.push(response);
-      });
+      })
+      .error(function(data, status) {
+  	    $scope.restrictedContent.push('Error:' + status);
+	    });
     }
 
     $scope.logout = function() {
